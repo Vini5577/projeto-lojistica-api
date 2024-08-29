6f37d7eea1b3104ddfb0cb725e98cb53f5060e77
Lyraa-Dev <lyra.dev001@gmail.com>
2024-08-29 20:16:19 -0300

    adicionando rotas de fornecedor

---
+++ src/main/java/com/api/stock/model/Fornecedor.java
@@ -0,0 +1,35 @@
+package com.api.stock.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tbl_fornecedor")
+public class Fornecedor {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    String nome;
+
+    @Column(nullable = false)
+    String telefone;
+
+    @Column(nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    String cnpj;
+
+    @Column(nullable = false)
+    private TipoServico tipoServico;
+}
